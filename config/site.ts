7ecebf721e40717c53f4d944d19cce1f46e4401f
Up-to-code <133324agh@gmail.com>
2025-01-02 14:45:02 +0200

    ad our logo

--- config/site.ts
+++ config/site.ts
@@ -4,14 +4,14 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  name: "Next Starter",
+  name: "ETJAH",
   description:
-    "Get your project off to an explosive start with Auth & User Roles! Harness the power of Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui to build your next big thing.",
+    "ETJAH is a project for managing your company",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
     twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/mickasmt/next-auth-roles-template",
+    github: "https://github.com/uptocodejs",
   },
   mailSupport: "support@next-starter.fake",
 };
